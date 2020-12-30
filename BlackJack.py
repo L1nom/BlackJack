652e6c22df484273e952bf02a239854d0d5e856b
Monil Patel <57769931+L1nom@users.noreply.github.com>
2020-12-30 01:37:31 -0500

    "Comments and code revision"

--- BlackJack.py
+++ BlackJack.py
@@ -1,6 +1,6 @@
 import random
 
-deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] * 8
+deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] * 1
 random.shuffle(deck)
 rules = """==============================================================================
 The rules of BlackJack are simple! Try and achieve a hand value of close but not over 21!
@@ -13,9 +13,13 @@ If you get over 21 points, you lose, and if the dealer gets over 21 points, you 
 
 
 def deal():
+    """
+    initialize each player's hand, deal cards from shuffled deck. Convert to
+    special cards based on value [11, 12, 13 or 1]
+    """
     player_hand = []
     for i in range(2):
-        player_card = deck.pop(0)
+        player_card = deck.pop()
         if player_card == 11:
             player_card = "J"
         elif player_card == 12:
@@ -25,10 +29,15 @@ def deal():
         elif player_card == 1:
             player_card = "A"
         player_hand.append(player_card)
+
     return player_hand
 
 
 def total(card_array):
+    """
+    Calculate the value of the player's hand. Jack, Queen, King are all 10, Ace can be value 1 or 11
+    based on user preference. Return total value to player
+    """
     total_val = 0
     for card in card_array:
         if card == "J" or card == "Q" or card == "K":
@@ -41,10 +50,14 @@ def total(card_array):
                 total_val += 11
         else:
             total_val += int(card)
+
     return total_val
 
 
 def hit(card_array):
+    """
+    Take player array and append a new card to it.
+    """
     new_card = deck.pop()
     if new_card == 11:
         new_card = "J"
@@ -55,11 +68,17 @@ def hit(card_array):
     elif new_card == 1:
         new_card = "A"
     card_array.append(new_card)
+
     return card_array
 
 
 def player(player_cards, player_total):
-    option = input("Do you want to hit or stay?").lower()
+    """
+    Give player the option to hit and add new cards or stand.
+    Calculate new value and return to user and ask to hit or stand recursively if
+    player value is <= 21, else the bust and ask to play again
+    """
+    option = input("Do you want to hit or stand?").lower()
     if option == "hit":
         player_cards = hit(player_cards)
         print(f"Your new hand is: {player_cards}")
@@ -73,12 +92,22 @@ def player(player_cards, player_total):
                 game()
             else:
                 exit()
-    elif option == "stay":
+    elif option == "stand":
         print(f"Your final total value is: {player_total}")
+    else:
+        print("Invalid option!")
+        player(player_cards, player_total)
+
     return player_total
 
 
 def dealer_ai(dealer_cards):
+    """
+    Calculate dealer value. Same steps as player, but Ace calculation is automatic.
+    If current total is <= 10, Ace = 11, else Ace = 1 to increase dealer chances
+    Dealer cannot hit if their value is over 16, but must hit if they are <= 16
+    Use recursion to accomplish dealer value until end conditions are met
+    """
     dealer_value = 0
     for card in dealer_cards:
         if card == "J" or card == "Q" or card == "K":
@@ -90,23 +119,25 @@ def dealer_ai(dealer_cards):
                 dealer_value += 1
         else:
             dealer_value += int(card)
-
     if dealer_value > 21:
         print("Dealer bust! You win")
         play_again()
-    elif dealer_value < 17:
+    if dealer_value < 17:
         print(f"The dealer has value of {dealer_value}\nDealer hits")
         dealer_cards = hit(dealer_cards)
         print(f"Dealer new hand is: {dealer_cards}")
-        dealer_value = dealer_ai(dealer_cards)
-    elif 17 <= dealer_value <= 21:
+        return dealer_ai(dealer_cards)
+    if 17 <= dealer_value <= 21:
         print(f"The dealer has value of {dealer_value}\nDealer must stand")
 
     return dealer_value
 
 
 def play_again():
-    player_choice = input("Would you like to play again? y/n").lower()
+    """
+    Give player the option to play again from user input
+    """
+    player_choice = input("Would you like to play again? [Y]es to continue, any other key to exit").lower()
     if player_choice == "y":
         game()
     else:
@@ -114,6 +145,11 @@ def play_again():
 
 
 def win_condition(player_final_total, dealer_total):
+    """
+    Define win conditions for player. Player wins if they draw or exceed dealer hand without
+    going over 21. At this point, we are assuming the totals are <=21 or else the game would
+    have ended during the value check function
+    """
     if dealer_total == player_final_total:
         print("You win by drawing with the dealer!")
         play_again()
@@ -126,6 +162,14 @@ def win_condition(player_final_total, dealer_total):
 
 
 def game():
+    """
+    Initialize deck for game. Deck can be increased or decreased by changing the value it is
+    multiplied by [a multiple of 4]. Deck gets reset every new game. Deal 2 to player and
+    dealer. Call game functions to do actions.
+    """
+    global deck
+    deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] * 8
+    random.shuffle(deck)
     print("=============================\nPlayer's turn")
     player_cards = deal()  # holds players first 2 cards
     dealer_cards = deal()  # holds dealers first 2 cards
@@ -135,15 +179,19 @@ def game():
     if player_total == 21:
         print("You got BlackJack! You win!")
         play_again()
-    print(f"The dealer is showing: {dealer_cards[0]}")
+    print(f"The dealer is showing: {dealer_cards[0]}")  # peek dealer's first card
     player_final_total = player(player_cards, player_total)
     print("=============================\nDealer's turn")
     print(f"The dealer has: {dealer_cards}")
     dealer_total = dealer_ai(dealer_cards)
     win_condition(player_final_total, dealer_total)
+    play_again()
 
 
 def introduction():
+    """
+    Game introduction. Give player the option to read rules or quit game before proceeding to play
+    """
     print("Welcome to BlackJack!")
     game_start = input("Would you like to [P]lay the game, read the [R]ules, or [Q]uit? ").lower()
     if game_start == "p":
